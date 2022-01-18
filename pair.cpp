66642cfcef92afc20d146ab8465153b5488df527
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2022-01-18 12:22:00 +0530

    Create pair.cpp

---
+++ pair.cpp
@@ -0,0 +1,20 @@
+//In pair we make group of two data-type or two containers.
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  
+    pair< int, string > p;
+    
+    p = make_pair ( 1, "soshubh" ); // first way to give value
+
+    p = { 2, "shubh" }; // second way to give value
+    
+    p.first=3;        //|
+    p.second="singh"; //| third way to give value
+    
+    pair< int, string > s=p; // copying p to s;
+    
+    cout<<p.first<<" "<<p.second;
+    
+    return 0;
+}
