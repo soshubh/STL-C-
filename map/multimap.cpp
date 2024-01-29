320965ad902766cd77cf11f88312dd962d8b8246
SHUBH SINGH <90331406+soshubh@users.noreply.github.com>
2024-01-29 12:43:11 +0530

    Create multimap.cpp

---
+++ map/multimap.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    multimap<int, int> mp;
+    // everything same as map, only it can store wultiple keys
+    // only map[key] can not be used here 
+    
+    return 0;
+}
