f17be8c9cdb191b1ad20b801f1c7b32dcc83fbff
SHUBH SINGH <90331406+soshubh@users.noreply.github.com>
2024-01-29 11:59:15 +0530

    Create set1.cpp

---
+++ set/set1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// store unique value in sorted oreder
+int main() {
+    set<int>st;
+    st.insert(1); // {1}
+    st.insert(2); // {1,2}
+    st.insert(3); // {1,2,3}
+    st.insert(4); // {1,2,3}
+    st.insert(1); // {1,2,3}
+    
+    // begin(), end(), rbegin(), rend(), size(), empty() and swap() are the same
+    
+    /*
+    auto it = st.find(2); // return iterator which point to the element 
+    auto it3 = st.find(5); // if element is not there it will return st.end()
+    */
+    
+    st.erase(2);
+    cout<<st.size();
+    cout<<st.count(1);/// if 1 is thre give 1 otherwise 0
+    
+    auto it1 = st.find(2); 
+    auto it2 = st.find(3); 
+    st.erase
+    
+    
+    
+    return 0;
+}
