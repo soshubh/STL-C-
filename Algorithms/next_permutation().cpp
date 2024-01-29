fa8edd1a2d7490fcbf2a6b1ec8df1c752b85d4a0
SHUBH SINGH <90331406+soshubh@users.noreply.github.com>
2024-01-29 17:07:15 +0530

    Create next_permutation().cpp

---
+++ Algorithms/next_permutation().cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "230";
+    do{
+        cout<<s<<"\n";
+    }while(next_permutation(s.begin(),s.end()));
+    
+    
+    return 0;
+}
